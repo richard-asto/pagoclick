ba1687dbee6bbf6c19cd9dce81b5761ac1d20ffd
richard-asto <SV09681015@idat.pe>
2026-05-28 19:39:02 -0500

    revision order.js

--- server/src/models/Order.js
+++ server/src/models/Order.js
@@ -1,73 +1,30 @@
-const { DataTypes } =
-  require("sequelize");
-
-const sequelize =
-  require("../config/db");
-
-const User =
-  require("./User");
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/db");
 
 const Order = sequelize.define(
   "Order",
   {
-
     id: {
       type: DataTypes.INTEGER,
-
       autoIncrement: true,
-
       primaryKey: true,
     },
-
     total: {
-      type: DataTypes.DECIMAL(
-        10,
-        2
-      ),
-
+      type: DataTypes.DECIMAL(10, 2),
       allowNull: false,
     },
-
     status: {
-      type: DataTypes.ENUM(
-        "pending",
-        "paid",
-        "refunded",
-        "cancelled"
-      ),
-
+      type: DataTypes.ENUM("pending", "paid", "refunded", "cancelled"),
       defaultValue: "pending",
     },
-
     paymentIntentId: {
       type: DataTypes.STRING,
     },
-
   },
   {
-
     tableName: "orders",
-
     timestamps: true,
   }
 );
-
-/*
-|--------------------------------------------------------------------------
-| RELATIONS
-|--------------------------------------------------------------------------
-*/
-User.hasMany(Order, {
-  foreignKey: "UserId",
-});
-
-Order.belongsTo(User, {
-  foreignKey: "UserId",
-});
-/*
-User.hasMany(Order);
-
-Order.belongsTo(User);
-*/
 
 module.exports = Order;
